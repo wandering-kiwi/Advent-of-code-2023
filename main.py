d3ea5f5b02dfbfcd946b6e6b327a659cb4501b97
wandering-kiwi <67548758+wandering-kiwi@users.noreply.github.com>
2023-12-03 09:53:18 +0000

    completed day2p1 in one line again and cleaned up file structure a bit

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-lst = list(filter(lambda x: x, [index+1 if not list(filter(lambda x: x, [i if (int(i[0]) > 12 and i[1]=='red') or (int(i[0]) > 13 and i[1]=='green') or (int(i[0]) > 14 and i[1]=='blue') else None for i in [k for sub in [[j[1:].split(' ') for j in i.split(',')] for i in txt.split(':')[1].split(';')] for k in sub]])) else None for index, txt in enumerate(open('day2.txt', 'r'))]))
-
-print(lst)
-
-
-for index, txt in enumerate(open('day2.txt', 'r')):
-  print(index+1)
-  if not list(filter(lambda x: x, [i if (int(i[0]) > 12 and i[1]=='red') or (int(i[0]) > 13 and i[1]=='green') or (int(i[0]) > 14 and i[1]=='blue') else None for i in [k for sub in [[j[1:].split(' ') for j in i.split(',')] for i in txt.split(':')[1].split(', ')] for k in sub]]))
-
+print(sum(list(filter(lambda x: x, [index+1 if not list(filter(lambda x: x, [i if (int(i[0]) > 12 and i[1].strip('\n')=='red') or (int(i[0]) > 13 and i[1].strip('\n')=='green') or (int(i[0]) > 14 and i[1].strip('\n')=='blue') else None for i in [k for sub in [[j[1:].split(' ') for j in i.split(',')] for i in txt.split(':')[1].split(';')] for k in sub]])) else None for index, txt in enumerate(open('day2.txt', 'r'))]))))
